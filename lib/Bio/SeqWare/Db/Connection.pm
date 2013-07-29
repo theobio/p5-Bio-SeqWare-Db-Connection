c7cffe65bb24412e46b52c5750fb228e6ce1955f
Stuart R. Jefferys <srjefferys@gmail.com>
2013-07-29 13:31:34 -0400

    Initial commit.

---
+++ lib/Bio/SeqWare/Db/Connection.pm
@@ -0,0 +1,117 @@
+package Bio::SeqWare::Db::Connection;
+
+use 5.008;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Bio::SeqWare::Db::Connection - Grab new SeqWare database connections easily.
+
+=head1 VERSION
+
+Version 0.000001
+
+=cut
+
+our $VERSION = '0.000001';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Bio::SeqWare::Db::Connection;
+
+    my $foo = Bio::SeqWare::Db::Connection->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Stuart R. Jefferys, C<srjefferys (at) gmail (dot) com>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-p5-bio-seqware-db-connection at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=p5-Bio-SeqWare-Db-Connection>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Bio::SeqWare::Db::Connection
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=p5-Bio-SeqWare-Db-Connection>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/p5-Bio-SeqWare-Db-Connection>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/p5-Bio-SeqWare-Db-Connection>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/p5-Bio-SeqWare-Db-Connection/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Stuart R. Jefferys.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see L<http://www.gnu.org/licenses/>.
+
+
+=cut
+
+1; # End of Bio::SeqWare::Db::Connection
---
+++ t/00-load.t
@@ -0,0 +1,13 @@
+#!perl -T
+use 5.008;
+use strict;
+use warnings FATAL => 'all';
+use Test::More;
+
+plan tests => 1;
+
+BEGIN {
+    use_ok( 'Bio::SeqWare::Db::Connection' ) || print "Bail out!\n";
+}
+
+diag( "Testing Bio::SeqWare::Db::Connection $Bio::SeqWare::Db::Connection::VERSION, Perl $], $^X" );
